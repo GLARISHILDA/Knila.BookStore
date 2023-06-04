7046a51448e0c68496197aae665e8f4a166edffe
GLARIS HILDA T <glaristhomas@gmail.com>
2023-06-04 13:01:24 +0530

    Added Data Definition

--- Knila.BookStore/Knila.BookStore.Domain/Book.cs
+++ Knila.BookStore/Knila.BookStore.Domain/Book.cs
@@ -2,5 +2,10 @@
 {
     public class Book
     {
+        public string Publisher { get; set; }
+        public string Title { get; set; }
+        public string AuthorLastName { get; set; }
+        public string AuthorFirstName { get; set; }
+        public decimal Price { get; set; }
     }
 }
